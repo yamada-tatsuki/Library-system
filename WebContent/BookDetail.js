3f00ab524a39c2242bcbffd714f394ebfbcd54ed
yamada_tatsuki <ta-yamada@virtualex.co.jp>
2020-05-20 12:23:10 +0900

    蔵書一覧詳細ページ(現場社員)

---
+++ WebContent/BookDetail.js
@@ -0,0 +1,81 @@
+// AjaxでJSONを取得する
+function executeAjax () {
+	'use strict';
+
+	var parameter  = location.search.substring( 1, location.search.length );
+	parameter = decodeURIComponent( parameter );
+	parameter = parameter.split('=')[1];
+
+	var requestQuery = { q : parameter} ;
+	console.dir(requestQuery);
+	$.ajax({
+		type : 'GET',
+		url : '/myFirstApp/api/bookdetail',
+		dataType : 'json',
+		data :requestQuery,
+		success : function (json) {
+			//console.log('返却値',data);
+
+			//for (var i = 0; i < json.length; i++) {
+
+				var element = json[1];
+
+				var param = GetQueryString();
+				console.log(param);
+				var id = param["id"];
+
+				$('#js-title').html(element.title);
+				$('#js-author').html(element.author);
+				$('#js-publisher').html(element.publisher);
+				$('#js-genre').html(element.genre);
+				$('#js-status').html(element.status);
+
+				//var record = '<tr>'
+				//	+ '<td>' + element.title + '</td>'
+					//+ '<td>' + element.author + '</td>'
+					//+ '<td>' + element.publisher + '</td>'
+					//+ '<td>' + element.genre + '</td>'
+					//+ '<td>' + element.status + '</td>'
+					//+ '</tr>';
+
+				//$('#table_data').append(record)
+			}
+		//}
+	});
+}
+
+function GetQueryString() {
+    var result = new Object();
+    if (1 < document.location.search.length) {
+        // 最初の1文字 (?記号) を除いた文字列を取得する
+        console.log(document.location.search);
+    	var query = document.location.search.substring(1);
+        console.log(query);
+        // クエリの区切り記号 (&) で文字列を配列に分割する
+        var parameters = query.split('&');
+        console.log(parameters);
+        for (var i = 0; i < parameters.length; i++) {
+            // パラメータ名とパラメータ値に分割する
+            var element = parameters[i].split('=');
+            console.log(element);
+            var paramName = decodeURIComponent(element[0]);
+            console.log(paramName);
+            var paramValue = decodeURIComponent(element[1]);
+            console.log(paramValue);
+            // パラメータ名をキーとして連想配列に追加する
+            result[paramName] = paramValue;
+        }
+        //result = {id: "EMP0001", name: "tanaka",age:"10"}
+    }
+    return result;
+}
+
+$(document).ready(function () {
+	'use strict';
+
+	// 初期表示用
+	executeAjax();
+
+	$('#table_data').ready('road',executeAjax);
+
+});
