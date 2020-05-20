44c75d19af9a921831baef4e1a181f1ce7aaef65
ohira_kaito <ka-ohira@virtualex.co.jp>
2020-05-20 11:45:07 +0900

    Add_仮完成

--- WebContent/Add.js
+++ WebContent/Add.js
@@ -9,10 +9,10 @@ var registItem = function () {
 	var requestQuery = {
 			itemTitle:inputItemTitle,
 			itemAuthor:inputItemAuthor,
-			ItemPublisher:inputItemPublisher,
-			ItemGenre:inputItemGenre,
-			ItemBoughtOn:inputItemBoughtOn,
-			ItemBoughtBy:inputItemBoughtBy
+			itemPublisher:inputItemPublisher,
+			itemGenre:inputItemGenre,
+			itemBoughtOn:inputItemBoughtOn,
+			itemBoughtBy:inputItemBoughtBy
 	};
 	console.log('requestQuery',requestQuery);
 	// サーバーにデータを送信する。
